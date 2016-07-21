0db83eac3345e52448f67172cd8d8d5610b00e5f
Baskar Murugesan <Baskar Murugesan>
2016-07-21 21:22:58 +0530

    Edited_Structure of Folders

--- index.php
+++ index.php
@@ -4,9 +4,15 @@
 		<title>Portfolio_Rebutton.co</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-<style> 
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="css/main.css" />
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/plugins/CSSPlugin.min.js"></script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/easing/EasePack.min.js"></script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenLite.min.js"></script>
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+<style>
+header
 #demo {
     width: 692px;
     height: 100px;
@@ -27,14 +33,37 @@
     background: url(http://rebutton.co/wp-content/uploads/2016/01/VECTOR_FIle-3-e1452970966884.png) no-repeat;
 }
 </style>
+	</head>
+	<body class="loading">
+		<div id="wrapper">
+			<div id="bg"></div>
+			<div id="overlay"></div>
+			<div id="main">
+
+				<!-- Header -->
+					<header id="header">
+						<h1>Portfolio</h1>
+						<p>Just Rebutton And Get Your Job Done</p>
+						<nav>
+							<ul>
+								<li><a href="#"><img src="images/img1.jpg" style="float:left;width:200px;height:200px;"></a></li>&nbsp;&nbsp;&nbsp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;
+								<li><a href="#"><img src="images/img2.jpg" style="float:left;width:200px;height:200px;"></a></li>&nbsp;&nbsp;&nbsp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;
+								<li><a href="#"><img src="images/img3.jpg" style="float:left;width:200px;height:200px;"></a></li>&nbsp;&nbsp;&nbsp;&ensp;&ensp;
+							</ul>
+						</nav>
+					</header>
+
+					<footer id="footer">
+						<span class="copyright">&copy; All Rights Reserved. &nbsp;<a href="http://rebutton.co">Rebutton.Co</a></span>
+					</footer>
+
+			</div>
+		</div>		
 <div id="demo">
     <div id="easeWrapper">
         <div id="logo"></div>
     </div>
 </div>
-		<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/plugins/CSSPlugin.min.js"></script>
-		<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/easing/EasePack.min.js"></script>
-		<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenLite.min.js"></script>
 		<script>
 			//we'll use a window.onload for simplicity, but typically it is best to use either jQuery's $(document).ready() or $(window).load() or cross-browser event listeners so that you're not limited to one.
 	window.onload = function() {
@@ -52,32 +81,7 @@
     SlowMo.ease
     */   
 	}
-	</script>
-	</head>
-	<body class="loading">
-		<div id="wrapper">
-			<div id="bg"></div>
-			<div id="overlay"></div>
-			<div id="main">
-					
-					<header id="header">
-						<h1>Portfolio</h1>
-						<p>Just Rebutton And Get Your Job Done</p>
-						<nav>
-							<ul>
-								<li><a href="#"><img src="binary-1332894.jpg" style="float:left;width:200px;height:200px;"></a></li>&nbsp;&nbsp;&nbsp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;
-								<li><a href="#"><img src="binary-1414319.jpg" style="float:left;width:200px;height:200px;"></a></li>&nbsp;&nbsp;&nbsp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;
-								<li><a href="#"><img src="girl-1328418.jpg" style="float:left;width:200px;height:200px;"></a></li>&nbsp;&nbsp;&nbsp;&ensp;&ensp;
-							</ul>
-						</nav>
-					</header>
-
-					<footer id="footer">
-						<span class="copyright">&copy; All Rights Reserved. &nbsp;<a href="http://rebutton.co">Rebutton.Co</a></span>
-					</footer>
-
-			</div>
-		</div>		
+		</script>
 		<script>
 			window.onload = function() { document.body.className = ''; }
 			window.ontouchmove = function() { return false; }
