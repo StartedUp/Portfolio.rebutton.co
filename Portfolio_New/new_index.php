574f9aed1eb9b6b602fe2e8ac2f4e747424c6931
Baskar Murugesan <Baskar Murugesan>
2016-07-24 17:31:09 +0530

    Images added and class defined

--- Portfolio_New/new_index.php
+++ Portfolio_New/new_index.php
@@ -7,43 +7,44 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js"></script>
     <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenLite.min.js"></script>
     <style>
-        body {overflow-x: hidden;
-            overflow-y: scroll;}
-            #logo {
-                position: relative;
-                width: 100%;
-                height: 100px;
-                background: url("images/vector_file_3.png") no-repeat;
-                background-size: 236px 75px;
-            }
-            ul {
-                list-style-type: none;
-                margin: 0;
-                padding: 0;
-            }
-
-            li {
-                display: inline;
-            }
-        </style>
+        /*body {
+            overflow-x: hidden;
+            overflow-y: scroll;
+        }*/
+        #logo {
+            position: relative;
+            width: 100%;
+            height: 100px;
+            background: url("images/vector_file_3.png") no-repeat;
+            background-size: 236px 75px;
+        }
+        #image1 {
+            background: url("images/img1.jpg") 100% / 150% no-repeat;
+        }
+        .portfolioImg{
+            background-color: red;
+            display:inline-block;
+            position: relative;
+            border-radius:10%;
+            width: 250px;
+            height: 200px;
+        }
+    </style>
     </head>
 
     <body>
         <div id="logo"></div>
-        <nav>
-            <ul>
-                <li><a href="#"><img src="images/img1.jpg" style="float:left;width:200px;height:200px;"></a></li>&nbsp;&nbsp;&nbsp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;
-                <li><a href="#"><img id="photo" src="images/img4.jpg" style="float:left;width:200px;height:200px;"></a></li>&nbsp;&nbsp;&nbsp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;
-                <li><a href="#"><img src="images/img5.jpg" style="float:left;width:200px;height:200px;"></a></li>&nbsp;&nbsp;&nbsp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;
-                <li><a href="#"><img src="images/img2.jpg" style="float:left;width:200px;height:200px;"></a></li>&nbsp;&nbsp;&nbsp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;
-            </ul>
-        </nav>
+            <a href="#"><div class="portfolioImg" id="image1"></div></a>
+            <a href="#"><div class="portfolioImg" id="image1"></div></a>
+            <a href="#"><div class="portfolioImg" id="image1"></div></a>
+
     </body>
     <script>
 //we'll use a window.onload for simplicity, but typically it is best to use either jQuery's $(document).ready() or $(window).load() or cross-browser event listeners so that you're not limited to one.
 window.onload = function() {
     var logo = document.getElementById("logo");
     TweenLite.to(logo, 2, {left:"80%", ease:Bounce.easeOut});
+    TweenMax.to(image1,2,{left:"20%", ease:Bounce.easeOut});
 
     /* Tip: Try substituting the following eases:
     Elastic.easeIn
