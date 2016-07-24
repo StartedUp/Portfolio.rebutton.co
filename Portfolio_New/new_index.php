80dacbb2c4906cfcc2a528baee245c860aa0d25d
PrithuGit <prithviprakashr@gmail.com>
2016-07-24 21:42:53 +0530

    Scroll effect added

--- Portfolio_New/new_index.php
+++ Portfolio_New/new_index.php
@@ -102,6 +102,17 @@ $(document).ready(function() {
     Power4.easeOut
     SlowMo.ease
     */   
+
+
+    $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1500);
+    }
+});
 });
 </script>
 </body>
